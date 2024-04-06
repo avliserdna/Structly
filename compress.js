a67e549289a3c2118403b494ec8472edbcdffc39
avliserdna <andre.christian.silva@gmail.com>
2024-04-05 22:40:07 -0500

    compress pairboarding.

---
+++ compress.js
@@ -0,0 +1,26 @@
+const compress = (s) => {
+  //two pointers
+  let i = 0;
+  let j = 0;
+  let results = [];
+
+  while(j <= s.length){
+      if(s[i] === s[j]){
+        j+=1
+      }else{
+        let string = s.slice(i,j)
+        if(string.length === 1){
+          results.push(s[i])
+          i = j
+        }else{
+            results.push(string.length,s[i])
+            i = j
+        }
+      }
+  }
+
+
+
+  return results.join('')
+
+};

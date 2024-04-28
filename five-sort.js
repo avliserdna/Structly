2f1f49a666c28d7f6abb0679f0273e98ea666561
avliserdna <andre.christian.silva@gmail.com>
2024-04-27 22:09:06 -0500

    finished five sort.

---
+++ five-sort.js
@@ -0,0 +1,22 @@
+const fiveSort = (nums) => {
+  // i =0 j = nums.length -1
+  let i = 0;
+  let j = nums.length-1
+  while (i < j) {
+    if (nums[j] === 5) {
+       j-=1
+    }
+    else if (nums[i] === 5) {
+      [nums[i], nums[j]] = [nums[j], nums[i]]
+      i+=1
+    }
+    else {
+      i+=1
+    }
+   }
+  return nums
+};
+
+module.exports = {
+  fiveSort,
+};
